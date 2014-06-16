2963d2b0f01301166a91a388fd3c11cd1107ec2d
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-16 15:35:17 +0800

    Assignment User Model: Created users migration and user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616072637) do
+ActiveRecord::Schema.define(version: 20140616073303) do
 
   create_table "posts", force: true do |t|
     t.string "url"
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
   end
 
 end
