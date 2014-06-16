3f77fecc741d96020a4f16ed0ba6b3b45801f4b3
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-16 15:42:36 +0800

    Assignment 1-M User and Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616073303) do
+ActiveRecord::Schema.define(version: 20140616073725) do
 
   create_table "posts", force: true do |t|
-    t.string "url"
-    t.string "title"
-    t.text   "description"
+    t.string  "url"
+    t.string  "title"
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
