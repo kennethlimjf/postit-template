84f11426c3496d8812c0113fc25b9dce03d42319
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-16 15:51:37 +0800

    Assignment Comment Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616073725) do
+ActiveRecord::Schema.define(version: 20140616074520) do
+
+  create_table "comments", force: true do |t|
+    t.text    "body"
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string  "url"
