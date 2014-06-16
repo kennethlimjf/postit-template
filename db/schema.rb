4b4ea81643c26502026d4fa830ff0f10b06b74a4
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-16 16:00:03 +0800

    Assignment Category Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616074520) do
+ActiveRecord::Schema.define(version: 20140616075400) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text    "body"
