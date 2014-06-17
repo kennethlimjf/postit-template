470deac7c0ab81a880665fdc5cbf2386b07e4562
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-18 00:58:06 +0800

    Assignment New Category Form

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,22 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'Category has been created.'
+      redirect_to new_category_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
