f7be56b56000f65466c730c606e535a112f180ea
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-18 02:54:22 +0800

    Capitalize category name before saving. Pluralized comments text in posts.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
+    @category.name.capitalize!
 
     if @category.save
       flash[:notice] = 'Category has been created.'
