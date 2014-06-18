1ccbabbde948870ba5eabc20df1fa369e506e584
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-18 17:33:08 +0800

    Assignment Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def fix_url(url_string)
+    url_string.include?('http://') ? url_string : "http://#{url_string}"
+  end
+
+  def datetime_formatter(datetime)
+    "#{distance_of_time_in_words_to_now(datetime)} ago at #{datetime.localtime.strftime("%d %b %Y,%l:%M %p (%Z%z)")}"
+  end
+
 end
