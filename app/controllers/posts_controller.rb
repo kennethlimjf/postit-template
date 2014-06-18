34255dc936e92b1b58f66d779bfa67dcad599c24
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-18 16:55:29 +0800

    Assignment Select categories on new/edit post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:title, :url, :description)
+      params.require(:post).permit(:title, :url, :description, category_ids: [])
     end
 end
