d53e4ce0bd5e77f060c48f0d80d4cf6469ecb94f
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-18 17:48:47 +0800

    Improved code after watching Assignment Helpers Solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,19 @@
 module ApplicationHelper
 
   def fix_url(url_string)
-    url_string.include?('http://') ? url_string : "http://#{url_string}"
+    url_string.starts_with?('http://') ? url_string : "http://#{url_string}"
   end
 
   def datetime_formatter(datetime)
-    "#{distance_of_time_in_words_to_now(datetime)} ago at #{datetime.localtime.strftime("%d %b %Y,%l:%M %p (%Z%z)")}"
+    if datetime
+      "#{distance_of_time_in_words_to_now(datetime)} ago at #{datetime.localtime.strftime("%d %b %Y,%l:%M %p (%Z%z)")}"
+    else
+      "at unknown time"
+    end
+  end
+
+  def display_user(user)
+    (user.nil?) ? 'Anonymous' : user.username
   end
 
 end
